1780b1fbf9f9c01f7544cae896d204551d7fcf9a
Ewertton Silva <ewertton45mb@gmail.com>
2021-05-08 00:32:26 -0300

    Novos Exercicios Java POO

---
+++ Aula06POO/src/aula06poo/Aula06POO.java
@@ -0,0 +1,19 @@
+package aula06poo;
+
+public class Aula06POO {
+
+    public static void main(String[] args) {
+    
+        ControleRemoto c1 = new ControleRemoto();
+        c1.ligar();
+        c1.maisVolume();
+        c1.ligarMudo();
+        c1.play();
+        c1.desligarMudo();
+        c1.pause();
+        c1.menosVolume();
+        c1.abrirMenu();
+        c1.fecharMenu();
+    }
+    
+}
